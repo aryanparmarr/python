7147c3d0c4696242a9179011ab2d637d38fb5d07
aryanparmarr <aryanparmarrr@gmail.com>
2025-02-21 17:14:56 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
-def nStarDiamond(n: int) -> None:
+def nStarDiamond(n):
     for i in range(1,n+1):
         print(" "*(n-i)+"*"*(2*i-1))
     
     for j in range(1,n+1):
         print(" "*(j-1) +"*"*( 2 * (n - j + 1) - 1))   
-    pass
+nStarDiamond(4)
