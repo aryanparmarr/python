a13a781031eb1cdb497882286559ebeba58b2cdd
aryanparmarr <aryanparmarrr@gmail.com>
2025-02-21 17:19:27 +0530

    Delete patterns.py

--- patterns.py
+++
@@ -1,7 +0,0 @@
-def nStarDiamond(n):
-    for i in range(1,n+1):
-        print(" "*(n-i)+"*"*(2*i-1))
-    
-    for j in range(1,n+1):
-        print(" "*(j-1) +"*"*( 2 * (n - j + 1) - 1))   
-nStarDiamond(4)
