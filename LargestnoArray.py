47eded37183640222b6a084cf7e7694a5ccbb369
aryanparmarr <aryanparmarrr@gmail.com>
2025-03-07 17:18:29 +0530

    Create LargestnoArray.py

---
+++ LargestnoArray.py
@@ -0,0 +1,13 @@
+def find_largest_element(arr):
+    
+    largest = arr[0]
+    
+    
+    for num in arr:
+        if num > largest:
+            largest = num
+    
+    return largest
+a ='aftgubnkm'
+for x in a:
+    print(x)
